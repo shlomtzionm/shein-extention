c4d7eb20f6cc81ce9e495e3abce5926413b352fc
shlomtzionm <shlomtzosh@gmail.com>
2025-05-25 00:05:44 +0300

    init

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -1,11 +1,41 @@
+import styles from "./index.module.css";
+import axios from "axios"
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "getText") {
-    const text = document.body.innerText
-    sendResponse(text)
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  if (message.action === "getProductData") {
+    const products = Array.from(document.querySelectorAll("section.product-card")) as HTMLElement[];
+
+    products.forEach(el => {
+      el.style.position = "relative";
+
+      const newDiv = document.createElement("div");
+      newDiv.textContent = "Hello from extension";
+      newDiv.className = styles.extensionVerify;  
+
+      el.appendChild(newDiv);
+    });
+const data = await isProductReview()
+console.log(data);
+
+sendResponse(products.map(el => el.outerHTML));
   }
-  // Return true to allow async `sendResponse`
-  return true
-})
+  return true;
+});
 
+async function isProductReview(){
+  try {
+    const url = "https://www.searchapi.io/api/v1/search";
+const params = {
+  "engine": "shein_product",
+  "product_id":"59968751",
+  "api_key":"yQoUGsxVdgSiZzQKuexkTaQP"
+};
 
+const response = await axios.get(url,{params})
+  console.log(response.data);
+  return response.data
+  } catch (error:any) {
+console.log(error);
+    
+  }
+}
--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -4,16 +4,23 @@ function IndexPopup() {
   async function display() {
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
-    const text = await chrome.tabs.sendMessage(tab.id!, {
-      action: "getText"
-    })
+  chrome.runtime.sendMessage({action:"getProductData"},(response)=>{
+    if (response.success){
+      console.log("Product data from api:", response.data);
+      
+    } else{
+      console.log( "Api error", response.error);
+      
+    }
+  })
+}
 
-    alert(text)
-  }
+
 
   return (
     <div style={{ padding: "1rem" }}>
-      <button onClick={display}>Get Page Text</button>
+      <button onClick={display}>turn on</button>
+      
     </div>
   )
 }
